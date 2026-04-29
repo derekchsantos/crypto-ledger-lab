6643a747969dd6c4e75b6931b6be58acf37f91c0
Derek Christopher Santos <dk.chsantos@gmail.com>
2026-04-29 16:33:59 -0300

    fix: resolve big number comparison in tests

--- test/TokenTest.js
+++ test/TokenTest.js
@@ -4,6 +4,10 @@ contract("FsocietyToken", (accounts) => {
   it("deve dar o suprimento total ao criador", async () => {
     const instance = await FsocietyToken.deployed();
     const balance = await instance.balanceOf(accounts[0]);
-    assert.equal(balance.toString(), (1000000 * 10**18).toString(), "Saldo inicial incorreto!");
+    
+    // 1 milhão com 18 zeros (10**18)
+    const expectedSupply = web3.utils.toWei("1000000", "ether"); 
+    
+    assert.equal(balance.toString(), expectedSupply, "Saldo inicial incorreto!");
   });
 });
