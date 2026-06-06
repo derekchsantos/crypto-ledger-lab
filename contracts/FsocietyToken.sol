a7ebc823bdda51b4cf21797e19b9db9d399e1589
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-06 16:49:12 -0300

    FsocietyToken contract

--- contracts/FsocietyToken.sol
+++ contracts/FsocietyToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//@author Derek Christopher 
 contract FsocietyToken is ERC20, Ownable {
     mapping(address => bool) public isBlacklisted;
 
